aa293bd245c478735e79d02f3f1eb8910755917c
nada1587 <nada1587@fnfcorp.com>
2022-02-04 13:21:05 +0900

    redux-logger 적용해 보기

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
+import logger from 'redux-logger'
 import rootReducer from "./reducers";
 import './index.css';
 import App from './App';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger)
+);
 
 ReactDOM.render(
   <Provider store={store}>

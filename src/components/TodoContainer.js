17c9950951af3ad20b2f87c8d3da83e07f243e11
nada1587 <nada1587@fnfcorp.com>
2022-02-08 14:28:05 +0900

    store 관련 주석 추가

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -5,6 +5,7 @@ import TodoList from './TodoList';
 import './TodoContainer.scss';
 
 const TodoContainer = () => {
+  // 여기서 state는 index.js에서 redux로 createStore할때 rootReducer 넣어준 initialState를 useSelector를 통해 접근
   const todos = useSelector(state => state.todos);
   const completed = useSelector(state => state.completed);
   return (
